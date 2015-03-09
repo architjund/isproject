d686e5d43f1f86511c6767c97bd1acd6843a239a
luqmannnn <luqmannurhakimt@gmail.com>
2015-03-09 13:42:50 +0800

    Update plot.java

--- plot.java
+++ plot.java
@@ -1,7 +1,31 @@
 import java
 public class Plot {
-  public static void main(String[] args) {
-    
+  private long startTme;
+  private double growthProgress;
+  private boolean isEmpty;
+  private Seed seed;
+  
+  public Plot(long startTime){
+    this.startTime=startTime;
+    growthProgress =0.0;
+    this.seed = null;
+    this.isEmpty = true;
+  }
+  
+  public void setStartTime (long startTime) {
+    this.startTime = startTime;
     
   }
+  
+  public long getStartTime () {
+    return startTime;
+  }
+  
+  public void setSeed (Seed s) {
+    seed=s;
+    !isEmpty;
+  }
+  
+  
+  
 }
